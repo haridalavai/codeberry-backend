ee98b9aed315cba5edfa1392a29daf92accc7ee4
haridalavai <dalaavai.hari@gmail.com>
2021-12-15 22:07:44 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = require('cors');
+
 // const mongoose = require('mongoose');
 const { MongoClient } = require('mongodb');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 

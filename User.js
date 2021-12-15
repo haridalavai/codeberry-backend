5b56da9a047bc622f027e14a31d63e0fdf76695f
haridalavai <dalaavai.hari@gmail.com>
2021-12-15 19:57:53 +0530

    initial commit

---
+++ User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const user_schema = new Schema({
+  name: { type: String },
+  designation: { type: String },
+  followers: { type: String },
+  following: { type: String },
+  posts: { type: String },
+});
+const User = mongoose.model('Users', user_schema);
+
+module.exports = { User };
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+// const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+// const { User } = require('./User');
+
+// const client = new MongoClient(
+//   `mongodb+srv://admin:fzbNl7OTapdgl4AB@cluster0.qhlhp.mongodb.net/usersc?retryWrites=true&w=majority`,
+// );
+
+let userss;
+
+async function main() {
+  const uri = `mongodb+srv://admin:fzbNl7OTapdgl4AB@cluster0.qhlhp.mongodb.net/usersc?retryWrites=true&w=majority`;
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await listDatabases(client);
+    await getUsers(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+const getUsers = async (client) => {
+  const users = await client
+    .db('usersc')
+    .collection('users')
+    .find({})
+    .toArray();
+  console.log(users);
+  userss = users;
+};
+main().catch(console.error);
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log('Databases:');
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+app.get('/users', async (req, res) => {
+  //   const users = await getUsers();
+  res.send(userss);
+});
+
+app.listen(3000 || process.env.port, () => {
+  console.log('server started');
+});
